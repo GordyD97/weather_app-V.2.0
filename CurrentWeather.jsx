1df45e34148cd4b0f8e5e7f2a1cedee5ec6dcb73
Gordon D <gordon.draine97@yahoo.com>
2023-08-09 23:58:25 -0400

    adding function to search bar

--- CurrentWeather.jsx
+++ CurrentWeather.jsx
@@ -2,6 +2,18 @@ import React from 'react'
 import { Container, Typography, Button, TextField, Divider} from '@mui/material'
 
 export const CurrentWeather = () => {
+  getWeather = async (e) => {
+    e.preventDefault();
+    const { city, country } = e.target.elements;
+    const cityValue = city.value;
+    const countryValue = country.value;
+
+    if (city.value && countryValue) {
+      const REACT_APP_API_URL = `https://api.openweathermap.org/data/2.5/weather?q=${cityValue},${countryValue}&appid=${REACT_APP_API_KEY}&units=metric`;
+      const response = await fetch(REACT_APP_API_URL);
+      const data = await response.json();
+      console.log(data)
+
   return (
     
       <Container>
